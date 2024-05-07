12a1f34e15c730c7435b95e1e618e8ef6b61eca3
Jeferson <jefer.hernandez1@gmail.com>
2024-05-07 08:30:05 -0500

    Este es una prueba

--- Ciclos/EjemplosCiclos/ejemploFor02.dart
+++ Ciclos/EjemplosCiclos/ejemploFor02.dart
@@ -1,5 +1,5 @@
 void main() {
-  print ("Imprimir los numeros del 0 al 25, de 2 en 2");   
+  print ("Imprimir los numeros del 0 al 25, de 2 en 2");  //Este comentario es prueba 
   for (int i = 0; i <= 25; i+=2) {
     print ("i: $i");
   }
