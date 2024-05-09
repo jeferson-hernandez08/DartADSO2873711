c9c624d43d8e93a75073b8d628557579392a701e
Jeferson <jefer.hernandez1@gmail.com>
2024-05-09 00:00:32 -0500

    Se avanza primera fase de ejercicio While03

--- Ciclos/EjemplosCiclos/ejemploWhile03.dart
+++ Ciclos/EjemplosCiclos/ejemploWhile03.dart
@@ -6,15 +6,15 @@ void main() {
     de los n numeros positivos.
   */
   
-  int cantNumeros = 5;
-  double num;
+  int sum = 0, contador = 0;
+  double cantNumeros;
 
-  print("Ingrese la cantidad de notas:" );
-  cantNumeros = int.parse(stdin.readLineSync()!);  //TEMINAMOS AQUI REALIZAR EJERCICIO COMO TAREA
+  print("Ingrese la cantidad de numeros positivos:" );
+  cantNumeros = double.parse(stdin.readLineSync()!);  //TEMINAMOS AQUI REALIZAR EJERCICIO COMO TAREA
 
-  while ( cantNumeros > 0 ) {
+  while ( contador < cantNumeros ) {
     print("Ingrese un numero " + (i + 1).toString());  //Se trata de hacer ejercicio.
-    num = double.parse(stdin.readLineSync()!);
+    num = double.parse(stdin.readLineSync()!);           //Se usa material de apoyo en internet
     if ( num % 2 == 0) {
     //Comprobar si es par
       print("El numero es par");
@@ -25,3 +25,19 @@ void main() {
   }
 }
 
+/*
+1. Inicio
+2. Leer el valor de n (la cantidad de números)
+3. Inicializar una variable suma a 0
+4. Inicializar una variable contador a 0
+5. Mientras contador < n hacer:
+     6. Leer un número
+     7. Si el número es positivo:
+          8. Sumar el número a la variable suma
+          9. Incrementar el contador en 1
+     10. Sino:
+          11. Mostrar un mensaje de error indicando que el número debe ser positivo
+12. Calcular el promedio como suma / n
+13. Mostrar el promedio
+14. Fin
+*/
