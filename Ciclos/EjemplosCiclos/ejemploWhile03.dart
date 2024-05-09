16a80ee5e16887905e6e4931769d1b4e80263cfb
Jeferson <jefer.hernandez1@gmail.com>
2024-05-09 11:58:01 -0500

    Se empieza a resolver taller

--- Ciclos/EjemplosCiclos/ejemploWhile03.dart
+++ Ciclos/EjemplosCiclos/ejemploWhile03.dart
@@ -6,38 +6,40 @@ void main() {
     de los n numeros positivos.
   */
   
-  int sum = 0, contador = 0;
-  double cantNumeros;
+  int cantNum, contador = 0, num, suma = 0;
+  double promedio;
 
-  print("Ingrese la cantidad de numeros positivos:" );
-  cantNumeros = double.parse(stdin.readLineSync()!);  //TEMINAMOS AQUI REALIZAR EJERCICIO COMO TAREA
+  print("Ingrese la cantidad de numeros:" );
+  cantNum = int.parse(stdin.readLineSync()!);  
 
-  while ( contador < cantNumeros ) {
-    print("Ingrese un numero " + (i + 1).toString());  //Se trata de hacer ejercicio.
-    num = double.parse(stdin.readLineSync()!);           //Se usa material de apoyo en internet
-    if ( num % 2 == 0) {                               //Esta es una prueba 
-    //Comprobar si es par
-      print("El numero es par");
+  while ( contador < cantNum ) {
+    print("Ingrese un numero positivo");                       
+    num = int.parse(stdin.readLineSync()!);         
+    if ( num > 0) {      
+      suma = suma + num;
+      contador++;                       
     }
     else {
-      print("El numero es impar");
+      print("El numero digitado no es positivo");
     }
   }
+  promedio = suma / contador;
+  print("El promedio es: $promedio");
 }
 
-/*
-1. Inicio
-2. Leer el valor de n (la cantidad de números)
-3. Inicializar una variable suma a 0
-4. Inicializar una variable contador a 0
-5. Mientras contador < n hacer:
-     6. Leer un número
-     7. Si el número es positivo:
-          8. Sumar el número a la variable suma
-          9. Incrementar el contador en 1
-     10. Sino:
-          11. Mostrar un mensaje de error indicando que el número debe ser positivo
-12. Calcular el promedio como suma / n
-13. Mostrar el promedio
-14. Fin
+/*          DIAGRAMA DE FLUJO 
+            1. Inicio
+            2. Leer el valor de n (la cantidad de números)
+            3. Inicializar una variable suma a 0
+            4. Inicializar una variable contador a 0
+            5. Mientras contador < n hacer:
+                6. Leer un número
+                7. Si el número es positivo:
+                      8. Sumar el número a la variable suma
+                      9. Incrementar el contador en 1
+                10. Sino:
+                      11. Mostrar un mensaje de error indicando que el número debe ser positivo
+            12. Calcular el promedio como suma / n
+            13. Mostrar el promedio
+            14. Fin
 */
---
+++ Ciclos/ejerciciosGuia4/For/ejercicioFor01.dart
@@ -0,0 +1,20 @@
+void main() {
+  //JEFERSON MAURICIO HERNANDEZ LADINO - EJERCICIO FOR 01
+  /*
+    1. Calcular el promedio de un alumno que tiene 7 calificaciones 
+       en la materia de Diseño Estructurado de Algoritmos.
+  */
+
+  //DECLARACION VARIABLES
+  
+
+  //ENTRADA DATOS.
+
+
+  //PROCESO - FORMULAS - SALIDA
+  
+
+
+
+
+}
