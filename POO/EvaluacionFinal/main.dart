4d477d4a6cd94f1ef6e8b386a958dbeda674ca74
Jeferson <jefer.hernandez1@gmail.com>
2024-09-24 18:10:31 -0500

    Se crean las mascotas perros y gatos y creacion de menu de mascotas (Evaluacion Final)

--- POO/EvaluacionFinal/main.dart
+++ POO/EvaluacionFinal/main.dart
@@ -1,47 +1,129 @@
 import 'dart:io';
+import 'Gato.dart';
 import 'Perro.dart';
 
 void main() {
+  String nombreMascota;   //Variables tanto de perros como gatos. Se pueden usar una solas para todas las mascotas.
+  int edadMascota;
+  String tipoMascota;
+  String razaMascota;
+  double pesoMascota;
+  String colorMascota;
+  String estirilizadoMascota;
+  int cantMascotas;
   List<Perro> listaPerros = [];
-  String nombrePerro;
-  int edadPerro;
-  String tipoMascotaPerro;
-  String razaPerro;
-  double pesoPerro;
-  String colorPerro;
-  String estirilizadoPerro;
-  int cantPerros;
+  List<Gato> listaGatos = [];
+  int opcion;
 
   print("Bienvenido a la veterinara!");
   print("Vamos a ingresar las mascotas perrunos:");
   print("Ingrese la cantidad de perros:");
-  cantPerros = int.parse(stdin.readLineSync()!);
+  cantMascotas = int.parse(stdin.readLineSync()!);
 
-  for (int i = 0; i < cantPerros; i++) {
-    print("Ingrese el nombre del perro #${i + 1}:");
-    nombrePerro = stdin.readLineSync()!;
-    print("Ingrese la edad del perro #${i+1} $nombrePerro:");
-    edadPerro = int.parse(stdin.readLineSync()!);
-    print("Ingrese el tipo de mascota del perro #${i+1} $nombrePerro:");
-    tipoMascotaPerro = stdin.readLineSync()!;
-    print("Ingresa la raza del perro #${i+1} $nombrePerro:");
-    razaPerro = stdin.readLineSync()!;
-    print("Ingrese el peso del perro #${i+1} $nombrePerro:");
-    pesoPerro = double.parse(stdin.readLineSync()!);
-    print("Ingrese el color del perro #${i+1} $nombrePerro:");
-    colorPerro = stdin.readLineSync()!;
-    print("Ingrese si el perro #${i+1} $nombrePerro esta estirilizado:");
-    estirilizadoPerro = stdin.readLineSync()!;
+  for (int i = 0; i < cantMascotas; i++) {
+    print("Ingrese el nombre del perro # ${i + 1}:");
+    nombreMascota = stdin.readLineSync()!;
+    print("Ingrese la edad del perro # ${i+1} $nombreMascota:");
+    edadMascota = int.parse(stdin.readLineSync()!);
+    print("Ingrese el tipo de mascota del perro # ${i+1} $nombreMascota:");
+    tipoMascota = stdin.readLineSync()!;
+    print("Ingresa la raza del perro # ${i+1} $nombreMascota:");
+    razaMascota = stdin.readLineSync()!;
+    print("Ingrese el peso del perro # ${i+1} $nombreMascota:");
+    pesoMascota = double.parse(stdin.readLineSync()!);
+    print("Ingrese el color del perro # ${i+1} $nombreMascota:");
+    colorMascota = stdin.readLineSync()!;
+    print("Ingrese si el perro # ${i+1} $nombreMascota esta estirilizado:");
+    estirilizadoMascota = stdin.readLineSync()!;
 
-    Perro perro = Perro(nombrePerro, edadPerro, tipoMascotaPerro, razaPerro, pesoPerro, colorPerro, estirilizadoPerro);
+    Perro perro = Perro(nombreMascota, edadMascota, tipoMascota, razaMascota, pesoMascota, colorMascota, estirilizadoMascota);
 
     listaPerros.add(perro);
-    //perro.mostrarInfoPerro();
-
-    // for (int i = 0; i < listaPerros.length; i++) {
-    //   perro.mostrarInfoPerro();
-    // }
-
+    print("Se ha creado el Perro # ${i + 1} satisfactoriamente:");
+    listaPerros[i].mostrarInfoPerro();
   }
- 
+
+  //Recorrer los elementos de la lista Objetos y llamar metodos. Mostrar informacion de cada elemento de la lista.
+  print("*" * 100);
+  print("Lista de perros creados:"); 
+  for(int i = 0; i < listaPerros.length; i++) {
+    print("Perro # ${i+1}:");
+    listaPerros[i].mostrarInfoPerro();
+  }
+  
+  //*****************Gatos********************//
+  print("*" * 100);
+  print("Vamos a ingresar las mascotas gatunas:");
+  print("Ingrese la cantidad de gatos:");
+  cantMascotas = int.parse(stdin.readLineSync()!);
+
+  for(int i = 0; i < cantMascotas; i++){
+    print("Ingrese el nombre del gato # ${i+1}:");
+    nombreMascota = stdin.readLineSync()!;
+    print("Ingrese la edad del gato # ${i+1} $nombreMascota:");
+    edadMascota = int.parse(stdin.readLineSync()!);
+    print("Ingrese el tipo de mascota del gato # ${i+1} $nombreMascota:");
+    tipoMascota = stdin.readLineSync()!;
+    print("Ingrese la raza del gato # ${i+1} $nombreMascota:");
+    razaMascota = stdin.readLineSync()!;
+    print("Ingrese el peso del gato # ${i+1} $nombreMascota:");
+    pesoMascota = double.parse(stdin.readLineSync()!);
+    print("Ingrese el color del gato # ${i+1} $nombreMascota:");
+    colorMascota = stdin.readLineSync()!;
+    print("Ingrese si el perro # ${i+1} $nombreMascota esta estirilizado:");
+    estirilizadoMascota = stdin.readLineSync()!;
+
+    Gato gato = Gato(nombreMascota, edadMascota, tipoMascota, razaMascota, pesoMascota, colorMascota, estirilizadoMascota);
+
+    listaGatos.add(gato);
+    print("Se ha creado el gato # ${i+1} satisfactoriamente:");
+    listaGatos[i].mostrasInfoGato();
+  }
+
+  //Recorrer los elementos de la lista Objetos y llamar metodos. Mostrar informacion de cada elemento de la lista.
+  print("*" * 100);
+  print("Lista de gatos creados:");
+  for (int i = 0; i < listaGatos.length; i++) {
+    print("Gato # ${i+1}:");
+    listaGatos[i].mostrasInfoGato();
+  } 
+
+  //Menu Mascotas. 
+  print("*" * 100);
+  do {
+    print("Menu de mascotas:");
+    print("1. Menu de Perros");
+    print("2. Menu de Gatos");
+    print("3. Para salir.");
+    opcion = int.parse(stdin.readLineSync()!);
+
+    switch (opcion) {
+      case 1:
+        print("*" * 100);
+        print("Lista de perros:");
+        for (int i = 0; i < listaPerros.length; i++) {
+          print("Perro # ${i+1}:");
+          listaPerros[i].mostrarInfoPerro();
+        }
+      break;
+      case 2:
+        print("*" * 100);
+        print("Lista de gatos:");
+        for (int i = 0; i < listaGatos.length; i++) {
+          print("Gato # ${i+1}:");
+          listaGatos[i].mostrasInfoGato();
+        }
+      break;
+      case 3:
+        print("Hasta la proxima");
+      break;
+      default:
+        print("Ingrese la opcion correcta");
+      break;
+    }
+  }
+  while (opcion != 3);
+
+
+
 }
